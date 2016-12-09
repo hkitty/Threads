056f084ca8254554a2ea0f8a6612b5544c0f64ea
hkitty <hkitty1488@gmail.com>
2016-12-09 21:09:48 +0200

    GUI

--- src/Form.java
+++ src/Form.java
@@ -13,6 +13,7 @@ public class Form extends JPanel {
 	private void initComponents() {
 		// JFormDesigner - Component initialization - DO NOT MODIFY  //GEN-BEGIN:initComponents
 		// Generated using JFormDesigner Evaluation license - Alan Walker
+		panelMain = new JPanel();
 		button1 = new JButton();
 		scrollPane1 = new JScrollPane();
 		textPane1 = new JTextPane();
@@ -21,49 +22,59 @@ public class Form extends JPanel {
 		button4 = new JButton();
 		button5 = new JButton();
 
-		//======== this ========
-
-		// JFormDesigner evaluation mark
-		setBorder(new javax.swing.border.CompoundBorder(
-			new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
-				"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
-				javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
-				java.awt.Color.red), getBorder())); addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
-
-		setLayout(new FormLayout(
-			"7*(default, $lcgap), default",
-			"default, $lgap, 111dlu, $lgap, default"));
-
-		//---- button1 ----
-		button1.setText("Stop");
-		add(button1, CC.xywh(1, 1, 15, 1));
-
-		//======== scrollPane1 ========
+		//======== panelMain ========
 		{
-			scrollPane1.setViewportView(textPane1);
+
+			// JFormDesigner evaluation mark
+			panelMain.setBorder(new javax.swing.border.CompoundBorder(
+				new javax.swing.border.TitledBorder(new javax.swing.border.EmptyBorder(0, 0, 0, 0),
+					"JFormDesigner Evaluation", javax.swing.border.TitledBorder.CENTER,
+					javax.swing.border.TitledBorder.BOTTOM, new java.awt.Font("Dialog", java.awt.Font.BOLD, 12),
+					java.awt.Color.red), panelMain.getBorder())); panelMain.addPropertyChangeListener(new java.beans.PropertyChangeListener(){public void propertyChange(java.beans.PropertyChangeEvent e){if("border".equals(e.getPropertyName()))throw new RuntimeException();}});
+
+			panelMain.setLayout(new FormLayout(
+				"7*(default, $lcgap), default",
+				"default, $lgap, 111dlu, $lgap, default"));
+
+			//---- button1 ----
+			button1.setText("Start");
+			panelMain.add(button1, CC.xywh(1, 1, 15, 1));
+
+			//======== scrollPane1 ========
+			{
+				scrollPane1.setViewportView(textPane1);
+			}
+			panelMain.add(scrollPane1, CC.xywh(1, 2, 15, 2));
+
+			//---- button2 ----
+			button2.setText("Thread");
+			panelMain.add(button2, CC.xy(1, 5));
+
+			//---- button3 ----
+			button3.setText("Done");
+			panelMain.add(button3, CC.xy(5, 5));
+
+			//---- button4 ----
+			button4.setText("Continue");
+			panelMain.add(button4, CC.xy(11, 5));
+
+			//---- button5 ----
+			button5.setText("Suspend");
+			panelMain.add(button5, CC.xy(15, 5));
 		}
-		add(scrollPane1, CC.xywh(1, 2, 15, 2));
-
-		//---- button2 ----
-		button2.setText("Thread");
-		add(button2, CC.xy(1, 5));
-
-		//---- button3 ----
-		button3.setText("Done");
-		add(button3, CC.xy(5, 5));
-
-		//---- button4 ----
-		button4.setText("Continue");
-		add(button4, CC.xy(11, 5));
-
-		//---- button5 ----
-		button5.setText("Suspend");
-		add(button5, CC.xy(15, 5));
 		// JFormDesigner - End of component initialization  //GEN-END:initComponents
+	}
+	
+	public static void main(String[] args){
+		JFrame frame = new JFrame("App");
+		frame.setContentPane(new Form().panelMain);
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	// JFormDesigner - Variables declaration - DO NOT MODIFY  //GEN-BEGIN:variables
 	// Generated using JFormDesigner Evaluation license - Alan Walker
+	private JPanel panelMain;
 	private JButton button1;
 	private JScrollPane scrollPane1;
 	private JTextPane textPane1;
